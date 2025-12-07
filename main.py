a4dcf7587241371381409fc8f468608ca5cdfa5b
Kamdem Ylanova <tankeuylanova@gmail.com>
2025-12-07 19:51:22 +0400

    feat: Add basic computer converter: bin, dec, hex

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+<<<<<<< HEAD
 my_age= int(input("Enter_your_age_in_years :"))
 age_in_days=(my_age*365)
 print(f"Your age in days is approximately {age_in_days} days")
 git init
+=======
+my_age= int(input("Enter_your_age_in_years :"))
+age_in_days=(my_age*365)
+print(f"Your age in days is approximately {age_in_days} days")
+>>>>>>> 63e8d67 (feat: Add basic computer converter bin, hex and dec)
